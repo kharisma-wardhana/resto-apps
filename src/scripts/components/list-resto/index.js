6662d72687ed034a7e2f2445f7be29881253883a
kharisma-wardhana <nanda.kharisma.29@gmail.com>
2020-07-29 08:08:28 +0700

    - implement list item

---
+++ src/scripts/components/list-resto/index.js
@@ -0,0 +1,26 @@
+class ListResto extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  set restorants(restorants) {
+    this._restorants = restorants;
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = '';
+    console.log(this._restorants);
+    this._restorants.map(_resto => {
+      const restoItem = document.createElement('item-resto');
+      restoItem.resto = _resto;
+      this.appendChild(restoItem);
+    });
+  }
+
+  renderError(message) {
+    this.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+  }
+}
+
+customElements.define('list-resto', ListResto);
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,3 +2,15 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import './components/app-bar/index.js';
 import './components/hero-element/index.js';
+import './components/list-resto/index.js';
+import './components/item-resto/index.js';
+import RestoData from './data/resto';
+
+const restoListElement = document.querySelector('list-resto');
+
+const renderList = async () => {
+  const results = await RestoData.getAllData();
+  restoListElement.restorants = results.restaurants;
+};
+
+document.addEventListener('DOMContentLoaded', renderList);
