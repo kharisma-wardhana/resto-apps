298de77b179e53df065dd67e440e212f873df427
Kharisma wardhana <nanda.kharisma29@gmail.com>
2020-08-23 11:28:32 +0700

    -implement manifest pwa generator

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 
 module.exports = {
@@ -49,8 +50,24 @@ module.exports = {
         },
       ],
     }),
+    new WebpackPwaManifest({
+      name: 'Foodies PWA',
+      short_name: 'Foodies',
+      description: 'Foodies Progressive Web App!',
+      background_color: '#33658a',
+      theme_color: '#33658a',
+      orientation: 'portrait',
+      scope: '/',
+      start_url: '/',
+      icons: [
+        {
+          src: path.resolve('src/public/images/cook.png'),
+          sizes: [120, 152, 167, 180, 192, 512] // multiple sizes
+        },
+      ]
+    }),
     new ServiceWorkerWebpackPlugin({
       entry: path.resolve(__dirname, 'src/scripts/sw.js'),
-    }),
+    })
   ],
 };
